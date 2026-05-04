81f929f73374dbfccdb8692470728fb36cd80fe2
Titas Eidikonis <55333436+yokz321@users.noreply.github.com.>
2026-05-04 14:09:10 +0300

    h

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,10 @@ import "./globals.css"
 import { Header } from "@/components/header"
 import { Footer } from "@/components/footer"
 import { ReactNode } from "react"
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Bibliotekos Sistema",
@@ -15,7 +19,7 @@ export default async function RootLayout({
   children: ReactNode
 }>) {
   return (
-    <html lang="lt">
+    <html lang="lt" className={cn("font-sans", geist.variable)}>
       <body className="container mx-auto max-w-screen-xl">
         <Header />
         {children}
