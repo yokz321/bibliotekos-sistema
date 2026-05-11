a21e3ec3ac51e87fa656b6762aaf245934b17a5d
Titas Eidikonis <55333436+yokz321@users.noreply.github.com.>
2026-05-11 11:40:30 +0300

    feat(borrowing): implement saveBorrowingAction and BorrowingService class for borrowing management

---
+++ actions/borrowing-actions.ts
@@ -0,0 +1,49 @@
+"use server"
+
+import { connectMongoose } from "@/utils/mongoose-client"
+import { Borrowing } from "@/models/borrowing-model"
+import { borrowingSchema, type BorrowingDTO } from "@/dto/borrowing-dto"
+import { revalidatePath } from "next/cache"
+import { Types } from "mongoose"
+
+export async function saveBorrowingAction(data: BorrowingDTO) {
+  const parse = borrowingSchema.safeParse(data)
+  if (!parse.success) {
+    return { success: false, error: "Užpildykite visus privalomus laukus!" }
+  }
+
+  const dto = parse.data
+  await connectMongoose()
+
+  try {
+    const today = new Date()
+
+    const overdueBorrowing = await Borrowing.findOne({
+      subscriberId: new Types.ObjectId(dto.subscriberId),
+      isReturned: false,
+      dueDate: { $lt: today },
+    })
+
+    if (overdueBorrowing) {
+      return {
+        success: false,
+        error:
+          "Abonentas negali pasiimti naujos knygos, nes turi vėluojančių negrąžintų knygų!",
+      }
+    }
+
+    await Borrowing.create({
+      subscriberId: new Types.ObjectId(dto.subscriberId),
+      bookId: new Types.ObjectId(dto.bookId),
+      borrowDate: new Date(dto.borrowDate),
+      dueDate: new Date(dto.dueDate),
+      isReturned: dto.isReturned,
+    })
+
+    revalidatePath("/reservations")
+    return { success: true }
+  } catch (error: unknown) {
+    console.error("KLAIDA SAUGANT REZERVACIJĄ:", error)
+    return { success: false, error: "Serverio klaida saugant duomenis" }
+  }
+}
---
+++ services/borrowing-service.ts
@@ -0,0 +1,34 @@
+import { Borrowing } from "@/models/borrowing-model"
+import { connectMongoose } from "@/utils/mongoose-client"
+import { Types } from "mongoose"
+
+export class BorrowingService {
+  async getAll() {
+    await connectMongoose()
+
+    const results = await Borrowing.find()
+      .populate("bookId")
+      .populate("subscriberId")
+      .sort({ createdAt: -1 })
+      .lean()
+
+    return JSON.parse(JSON.stringify(results))
+  }
+
+  async returnBook(id: string) {
+    await connectMongoose()
+
+    await Borrowing.updateOne(
+      { _id: new Types.ObjectId(id) },
+      {
+        isReturned: true,
+        returnDate: new Date(),
+      }
+    )
+  }
+
+  async delete(id: string) {
+    await connectMongoose()
+    await Borrowing.deleteOne({ _id: new Types.ObjectId(id) })
+  }
+}
